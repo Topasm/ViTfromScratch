13b9b91aa58da2c233302a87ffa9435f1f48ae55
Topasm <dhkdwk0215@naver.com>
2025-04-14 13:54:29 +0900

    fix typos

--- model/model.py
+++ model/model.py
@@ -3,43 +3,30 @@ import torch.nn as nn
 from modules import TransformerEncoder, ClassificationHeadMLP
 
 
-class Vit(nn.module):
+class Vit(nn.Module):
 
     """ViT-Base Layer 12  Hidden size 768 MLP 3072 Head 12 86M"""
 
-    def __init__(self, input_dim, num_head, num_patch, num_class):
-        self.cls = nn.Embedding(num_patch, num_class)
+    def __init__(self, input_dim=768, num_head=12, num_patch=256, num_class=10):
+        super().__init__()
+        self.cls = nn.Parameter(num_patch, num_class)
 
-        self.pose_embedding = nn.Embedding(num_patch, latent_dim)
+        self.pose_embedding = nn.Embedding(num_patch+1, latent_dim)
 
-        self.TransformerEncoderLayer1 = TransformerEncoder(
+        self.Transformer = nn.Sequential(*[TransformerEncoder(
             input_dim=input_dim, num_head=num_head)
-        self.TransformerEncoderLayer2 = TransformerEncoder(
-            input_dim=input_dim, num_head=num_head)
-        self.TransformerEncoderLayer3 = TransformerEncoder(
-            input_dim=input_dim, num_head=num_head)
-        self.TransformerEncoderLayer4 = TransformerEncoder(
-            input_dim=input_dim, num_head=num_head)
-        self.TransformerEncoderLayer5 = TransformerEncoder(
-            input_dim=input_dim, num_head=num_head)
-        self.TransformerEncoderLayer6 = TransformerEncoder(
-            input_dim=input_dim, num_head=num_head)
-        self.TransformerEncoderLayer7 = TransformerEncoder(
-            input_dim=input_dim, num_head=num_head)
-        self.TransformerEncoderLayer8 = TransformerEncoder(
-            input_dim=input_dim, num_head=num_head)
-        self.TransformerEncoderLayer9 = TransformerEncoder(
-            input_dim=input_dim, num_head=num_head)
-        self.TransformerEncoderLayer10 = TransformerEncoder(
-            input_dim=input_dim, num_head=num_head)
-        self.TransformerEncoderLayer11 = TransformerEncoder(
-            input_dim=input_dim, num_head=num_head)
-        self.TransformerEncoderLayer12 = TransformerEncoder(
-            input_dim=input_dim, num_head=num_head)
-        self.MLP_Head = ClassificationHeadMLP(inputdim=input_dim, outdim=)
+            for _ in range(12)])
+        self.MLP_Head = ClassificationHeadMLP(
+            inputdim=input_dim, outdim=num_class)
 
     def forward(self, x):
 
-        x_cls = torch.concat([x, cls], dim=1)
+        x_cls = torch.concat([x, self.cls], dim=1)
 
         x_pose = x_cls + self.pose_embedding
+
+        x_t_out = self.Transformer(x_pose)
+
+        cls_ouput = x[:-1]
+        out = self.MLP_Head(cls_ouput)
+        return out
