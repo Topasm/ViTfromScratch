42f93fd48ecb47f51c9d6bc250ec044408a4e06c
Topasm <dhkdwk0215@naver.com>
2025-04-14 13:47:26 +0900

    save

--- model/model.py
+++ model/model.py
@@ -1,5 +1,45 @@
 import torch
 import torch.nn as nn
+from modules import TransformerEncoder, ClassificationHeadMLP
 
 
-class Vit(nn.module)
+class Vit(nn.module):
+
+    """ViT-Base Layer 12  Hidden size 768 MLP 3072 Head 12 86M"""
+
+    def __init__(self, input_dim, num_head, num_patch, num_class):
+        self.cls = nn.Embedding(num_patch, num_class)
+
+        self.pose_embedding = nn.Embedding(num_patch, latent_dim)
+
+        self.TransformerEncoderLayer1 = TransformerEncoder(
+            input_dim=input_dim, num_head=num_head)
+        self.TransformerEncoderLayer2 = TransformerEncoder(
+            input_dim=input_dim, num_head=num_head)
+        self.TransformerEncoderLayer3 = TransformerEncoder(
+            input_dim=input_dim, num_head=num_head)
+        self.TransformerEncoderLayer4 = TransformerEncoder(
+            input_dim=input_dim, num_head=num_head)
+        self.TransformerEncoderLayer5 = TransformerEncoder(
+            input_dim=input_dim, num_head=num_head)
+        self.TransformerEncoderLayer6 = TransformerEncoder(
+            input_dim=input_dim, num_head=num_head)
+        self.TransformerEncoderLayer7 = TransformerEncoder(
+            input_dim=input_dim, num_head=num_head)
+        self.TransformerEncoderLayer8 = TransformerEncoder(
+            input_dim=input_dim, num_head=num_head)
+        self.TransformerEncoderLayer9 = TransformerEncoder(
+            input_dim=input_dim, num_head=num_head)
+        self.TransformerEncoderLayer10 = TransformerEncoder(
+            input_dim=input_dim, num_head=num_head)
+        self.TransformerEncoderLayer11 = TransformerEncoder(
+            input_dim=input_dim, num_head=num_head)
+        self.TransformerEncoderLayer12 = TransformerEncoder(
+            input_dim=input_dim, num_head=num_head)
+        self.MLP_Head = ClassificationHeadMLP(inputdim=input_dim, outdim=)
+
+    def forward(self, x):
+
+        x_cls = torch.concat([x, cls], dim=1)
+
+        x_pose = x_cls + self.pose_embedding
--- model/modules.py
+++ model/modules.py
@@ -6,16 +6,21 @@ import torch.nn.functional as F
 class EncoderMLP(nn.Module):
     """mlp in vit
 
+    The MLP contains two layers with a GELU non-linearity
 
     mlp size is 3072 in 5p table 1"""
 
     def __init__(self, inputdim, hiddendim):
         super().__init__()
         self.mlp = nn.Sequential(nn.Linear(inputdim, hiddendim),
-                                 nn.ReLU())
+                                 nn.GELU(),
+                                 nn.Linear(hiddendim, hiddendim),
+                                 nn.GELU)
 
     def forward(self, x):
         x = self.mlp(x)
+
+        return x
 
 
 class ClassificationHeadMLP(nn.Module):
@@ -57,35 +62,56 @@ class SA(nn.Module):
 class MultiheadAttention(nn.Module):
     """ multihead Attention"""
 
-    def __init__(self, input, num_head):
+    def __init__(self, embedded_dim, num_head):
         super().__init__()
 
         self.num_head = num_head
 
-        self.q_proj = nn.Linear(input, ouput)
-        self.k_proj = nn.Linear(input, ouput)
-        self.v_proj = nn.Linear(input, ouput)
+        self.q_proj = nn.Linear(embedded_dim, embedded_dim)
+        self.k_proj = nn.Linear(embedded_dim, embedded_dim)
+        self.v_proj = nn.Linear(embedded_dim, embedded_dim)
+
+        self.output_proj = nn.Linear()
 
     def forward(self, x):
 
         B, N, D = x.shape
 
-        qkv = self.U_msa(x)
-        q, k, v = qkv.chunk(self.num_head, dim=-1)
+        q = self.q_proj(x)
+        k = self.k_proj(x)
+        v = self.v_proj(x)
 
         Attention = F.softmax(q @ k.transpose(-2, -1) / (D**0.5))
 
-        SAout = Attention@v
+        MHAout = Attention@v
 
-        return SAout
+        MHAout = self.output_proj(MHAout)
+
+        return MHAout
 
 
 class TransformerEncoder(nn.Module):
     """ Transformer Encoder
 
-     but replace the Batch Normalization lay-
-    ers (Ioffe & Szegedy, 2015) with Group Normalization """
+    Transformer encoder (Vaswani et al., 2017) consists of alternating layers of multiheaded self-
+attention (MSA, see Appendix A) and MLP blocks (Eq. 2, 3). Layernorm (LN) is applied before
+every block, and residual connections after every block """
 
-    def __init__(self, input):
+    "embedded patchs 16x16 B, N, D, C"
+
+    def __init__(self, input_dim, num_head):
         super().__init__()
-        self.norm = nn.GroupNorm(num_groups=32, num_channels=3)
+        self.norm1 = nn.LayerNorm(num_groups=32, num_channels=3)
+        self.mha = MultiheadAttention(input_dim, num_head=num_head)
+        self.norm2 = nn.LayerNorm(num_groups=32, num_channels=3)
+        self.mlp = EncoderMLP(input_dim, input_dim)
+
+    def forward(self, x):
+        x_norm = self.norm1(x)
+        x_attn = self.mha(x_norm)
+        x_update = x + x_attn
+        x_update_norm = self.norm2(x_update)
+        x_out = self.mlp(x_update_norm)
+        x_out = x_out + x_update
+
+        return x_out
