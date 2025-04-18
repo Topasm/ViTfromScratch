821621acf3efec8662313f7cdbc190ead232aeb4
Topasm <dhkdwk0215@naver.com>
2025-04-18 12:25:16 +0900

    add cosine scheduler

--- train.py
+++ train.py
@@ -1,72 +1,108 @@
 import torch
 import torch.nn as nn
-from torch.optim import Adam
+from torch.optim import AdamW
+import torch.optim.lr_scheduler as lr_scheduler
 import numpy as np
 from torchvision import transforms, datasets
 from model.model import Vit
 import wandb
 
 device = "cuda"
-path = './cifar_vit.pth'
+save_dir = './checkpoints'
+save_path = './cifar_vit.pth'
 
-"https://tutorials.pytorch.kr/beginner/basics/data_tutorial.html"
-"https://tutorials.pytorch.kr/beginner/blitz/cifar10_tutorial.html"
 transform = transforms.Compose([
     transforms.ToTensor(),
-    transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
+    transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
+])
 
 batch_size = 256
 total_epoch = 200
+warmup_epochs = 10
 
-trainset = datasets.CIFAR10(root='./data', train=True,
-                            download=True, transform=transform)
-trainloader = torch.utils.data.DataLoader(trainset, batch_size=batch_size,
-                                          shuffle=True, num_workers=2)
+trainset = datasets.CIFAR10(
+    root='./data', train=True, download=True, transform=transform)
+trainloader = torch.utils.data.DataLoader(
+    trainset, batch_size=batch_size, shuffle=True, num_workers=2)
 
 testset = datasets.CIFAR10(root='./data', train=False,
                            download=True, transform=transform)
-testloader = torch.utils.data.DataLoader(testset, batch_size=batch_size,
-                                         shuffle=False, num_workers=2)
+testloader = torch.utils.data.DataLoader(
+    testset, batch_size=batch_size, shuffle=False, num_workers=2)
 
 wandb.init(project="vit_fromscratch")
 
 
 def train():
-
     configs = {
         "patch_size": 4,
-        "num_class": 10}
+        "num_class": 10
+    }
 
     model = Vit(**configs).to(device)
     criterion = nn.CrossEntropyLoss()
-    optimizer = Adam(model.parameters(), lr=1e-3,
-                     betas=(0.9, 0.999), weight_decay=0.1)
+    optimizer = AdamW(model.parameters(), lr=5e-4,
+                      betas=(0.9, 0.999), weight_decay=0.05)
 
-    """We train all models, including ResNets, using Adam (Kingma & Ba,
-2015) with β1 = 0.9, β2 = 0.999, a batch size of 4096 and apply a high weight decay of 0.1, """
+    # Warmup + Cosine Annealing Scheduler
+    scheduler = lr_scheduler.CosineAnnealingLR(
+        optimizer, T_max=total_epoch - warmup_epochs)
+
+    # Warmup manual setting
+    def warmup_lr_lambda(current_epoch):
+        if current_epoch < warmup_epochs:
+            return float(current_epoch) / float(max(1, warmup_epochs))
+        return 1.0
+
+    warmup_scheduler = lr_scheduler.LambdaLR(
+        optimizer, lr_lambda=warmup_lr_lambda)
 
     for epoch in range(total_epoch):
+        model.train()
         running_loss = 0.0
-        for i, data in enumerate(trainloader, 0):
+        correct = 0
+        total = 0
 
-            inputs, lables = data
-            inputs, lables = inputs.to(device), lables.to(device)
+        for i, data in enumerate(trainloader, 0):
+            inputs, labels = data
+            inputs, labels = inputs.to(device), labels.to(device)
 
             optimizer.zero_grad()
             outputs = model(inputs)
-            loss = criterion(outputs, lables)
+            loss = criterion(outputs, labels)
             loss.backward()
             optimizer.step()
 
             running_loss += loss.item()
-            wandb.log({"loss": loss.item(), "epoch": epoch})
+
+            _, predicted = outputs.max(1)
+            total += labels.size(0)
+            correct += predicted.eq(labels).sum().item()
+
+        train_loss = running_loss / len(trainloader)
+        train_acc = 100. * correct / total
+
+        wandb.log({
+            "loss": train_loss,
+            "train_accuracy": train_acc,
+            "epoch": epoch,
+            "lr": optimizer.param_groups[0]["lr"]
+        })
 
         print(
-            f"Epoch [{epoch+1}/{total_epoch} - Loss: {running_loss/len(trainloader)}]")
+            f"Epoch [{epoch+1}/{total_epoch}] - Loss: {train_loss:.4f} - Train Acc: {train_acc:.2f}%")
 
-        if (epoch+1) % 10 == 0:
-            torch.save(model.state_dict(), f"./cifar_vit_epoch{epoch+1}.pth")
-        wandb.save(path)
+        # Save model every 10 epochs
+        if (epoch + 1) % 10 == 0:
+            ckpt_path = f"{save_dir}/cifar_vit_epoch{epoch+1}.pth"
+            torch.save(model.state_dict(), ckpt_path)
+            wandb.save(ckpt_path)
+
+        # Scheduler step
+        if epoch < warmup_epochs:
+            warmup_scheduler.step()
+        else:
+            scheduler.step()
 
 
 def main():
